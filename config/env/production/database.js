2a6bf1f11784872de7ec47bb4d11c1d6b96dc0a6
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2022-03-02 14:59:32 -0500

    commit2

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,17 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-module.exports = () => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
         ssl: {
-          rejectUnauthorized: false,
+          ca: env('DATABASE_CA') 
         },
       },
-      options: {},
+      debug: false,
     },
-  },
-});
+  });
